93409c79a9e38bc58e741f05e0e75695a8ee7c36
Dan Brubaker Horst <dbrubak1@nd.edu>
2012-06-12 13:48:06 -0400

    HYGALL-248: Additional attributes for collection customization

--- app/models/atrium/collection.rb
+++ app/models/atrium/collection.rb
@@ -34,20 +34,16 @@ class Atrium::Collection < ActiveRecord::Base
     search_facets.map{|facet| facet.name }
   end
 
-  def custom_title
-    nil # TODO: return a user-provided chunck of HTML
-  end
-
-  def has_custom_title?
-    false # TODO: check to see if custom HTML is provided for the main colleciton title
-  end
-
   def pretty_title
     title.blank? ? 'Unnamed Collection' : title
   end
 
-  def title_markup
-    has_custom_title? ? custom_title : "<h2>#{title}</h2>".html_safe
+  def has_custom_title?
+    !title_markup.blank?
+  end
+
+  def display_title
+    has_custom_title? ? title_markup.html_safe : "<h2>#{title}</h2>".html_safe
   end
 
   def search_facet_names=(collection_of_facet_names)
--- lib/generators/atrium/templates/migrations/create_atrium_collections.rb
+++ lib/generators/atrium/templates/migrations/create_atrium_collections.rb
@@ -2,12 +2,15 @@ class CreateAtriumCollections < ActiveRecord::Migration
   def self.up
     create_table :atrium_collections do |t|
       t.string :title
+      t.string :url_slug
       t.string :filter_query_params
       t.string :theme
+      t.text :title_markup
       t.text :collection_description
       t.text :collection_items
     end
     add_index :atrium_collections, :id
+    add_index :atrium_collections, :url_slug
   end
 
   def self.down
