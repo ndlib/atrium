1151cbd3a5b1445ef4ce4fdd7f5d0a54cc741715
Dan Brubaker Horst <dbrubak1@nd.edu>
2012-06-21 14:53:49 -0400

    Migrating methods into new Engine class

--- lib/atrium/engine.rb
+++ lib/atrium/engine.rb
@@ -3,5 +3,12 @@ require "rails"
 
 module Atrium
   class Engine < Rails::Engine
+
+    # AtriumHelper is needed by all helpers, so we inject it
+    # into action view base here.
+    initializer 'atrium.helpers' do |app|
+      ActionView::Base.send :include, AtriumHelper
+    end
+
   end
 end
