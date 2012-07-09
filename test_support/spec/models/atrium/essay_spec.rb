b42911afaeed0fa57ca34d4275e88940028af99a
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-07-09 11:24:19 -0400

    Added Atrium::Essay specs

---
+++ test_support/spec/models/atrium/essay_spec.rb
@@ -0,0 +1,12 @@
+require File.expand_path(File.dirname(__FILE__) + '/../../spec_helper')
+
+describe Atrium::Essay do
+
+  it { should belong_to :description }
+
+  it { should be_accessible :content }
+  it { should be_accessible :content_type }
+
+  it { should validate_presence_of :atrium_description_id }
+
+end
