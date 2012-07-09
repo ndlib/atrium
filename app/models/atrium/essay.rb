3bf5eacdf6ab4980d3fd1e73002eacf263145d97
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-07-09 11:23:57 -0400

    Reworking Atrium::Essay by moving methods to group concepts together.
    While the refactor looks big, I only rearranged methods – and declared one
    private. The idea being that I want to group together the methods that interact
    on the same aspect of the class.

--- app/models/atrium/essay.rb
+++ app/models/atrium/essay.rb
@@ -1,12 +1,19 @@
 class Atrium::Essay < ActiveRecord::Base
   self.table_name = 'atrium_essays'
 
-  belongs_to :description, :class_name => 'Atrium::Description', :foreign_key => 'atrium_description_id'
+  belongs_to(
+    :description,
+    :class_name => 'Atrium::Description',
+    :foreign_key => 'atrium_description_id'
+  )
 
   validates_presence_of :atrium_description_id
 
-  attr_accessible :content_type, :content , :atrium_description_id
-  attr_accessible :content_type, :content , :atrium_description_id
+  attr_accessible(
+    :content_type,
+    :content ,
+    :atrium_description_id
+  )
 
   def blank?
     content.blank?
