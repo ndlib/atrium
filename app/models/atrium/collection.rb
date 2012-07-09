39cf2d2543eca50f924a270d54577371f0810da8
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-07-09 09:36:07 -0400

    Reworking Atrium::Collection by moving methods to group concepts together.
    While the refactor looks big, I only rearranged methods – and declared one
    private. The idea being that I want to group together the methods that interact
    on the same aspect of the class.

--- app/models/atrium/collection.rb
+++ app/models/atrium/collection.rb
@@ -1,52 +1,37 @@
 class Atrium::Collection < ActiveRecord::Base
   self.table_name = 'atrium_collections'
 
-  has_many :exhibits,      :class_name => 'Atrium::Exhibit',       :foreign_key => 'atrium_collection_id', :order => 'set_number ASC', :dependent => :destroy
-  has_many :search_facets, :class_name => 'Atrium::Search::Facet', :foreign_key => 'atrium_collection_id', :dependent => :destroy
-  has_many :showcases,     :class_name => 'Atrium::Showcase',      :as => :showcases
+  attr_accessible(
+    :collection_items,
+    :filter_query_params,
+    :theme,
+    :title,
+    :title_markup,
+    :collection_description,
+    :search_instructions,
+    :search_facet_names,
+    :url_slug,
+    :exhibits_attributes,
+    :search_facets_attributes
+  )
 
-  serialize :filter_query_params
-  serialize :collection_items, Hash
+  has_many(
+    :showcases,
+    :class_name => 'Atrium::Showcase',
+    :as => :showcases
+  )
 
-  attr_accessible :collection_items, :filter_query_params, :theme, :title, :title_markup, :collection_description, :search_instructions, :search_facet_names,:url_slug, :exhibits_attributes, :search_facets_attributes
+  has_many(
+    :search_facets,
+    :class_name => 'Atrium::Search::Facet',
+    :foreign_key => 'atrium_collection_id',
+    :dependent => :destroy
+  )
 
-  accepts_nested_attributes_for :exhibits,      :allow_destroy => true
   accepts_nested_attributes_for :search_facets, :allow_destroy => true
-  #attr_accessible :exhibits_attributes, :search_facets_attributes
-  #attr_accessible :exhibits_attributes, :search_facets_attributes
-
-  @@included_themes = ['default']
-
-  def self.available_themes
-    return @@available_themes if defined? @@available_themes
-    # NOTE: theme filenames should conform to rails expectations and only use periods to delimit file extensions
-    local_themes = Dir.entries(File.join(Rails.root, 'app/views/layouts/atrium_themes')).reject {|f| f =~ /^\./}
-    local_themes.collect!{|f| f.split('.').first}
-    @@available_themes = @@included_themes + local_themes
-  end
-
-  def collection_items
-    read_attribute(:collection_items) || write_attribute(:collection_items, {})
-  end
-
-  def solr_doc_ids
-    collection_items[:solr_doc_ids] unless collection_items.blank?
-  end
 
   def search_facet_names
     search_facets.map{|facet| facet.name }
-  end
-
-  def pretty_title
-    title.blank? ? 'Unnamed Collection' : title
-  end
-
-  def has_custom_title?
-    !title_markup.blank?
-  end
-
-  def display_title
-    has_custom_title? ? title_markup.html_safe : "<h2>#{title}</h2>".html_safe
   end
 
   def search_facet_names=(collection_of_facet_names)
@@ -54,6 +39,32 @@ class Atrium::Collection < ActiveRecord::Base
     add_collection_of_facets_by_name( collection_of_facet_names - existing_facet_names )
     remove_collection_of_facets_by_name( existing_facet_names - collection_of_facet_names )
   end
+
+  def add_collection_of_facets_by_name(collection_of_facet_names)
+    collection_of_facet_names.each do |name|
+      search_facets << Atrium::Search::Facet.find_or_create_by_name_and_collection_id(name, id)
+    end
+  end
+  private :add_collection_of_facets_by_name
+
+  def remove_collection_of_facets_by_name(collection_of_facet_names)
+    collection_of_facet_names.each do |name|
+      search_facets.delete(Atrium::Search::Facet.find_by_name_and_collection_id(name, id))
+    end
+  end
+  private :remove_collection_of_facets_by_name
+  has_many(
+    :exhibits,
+    :class_name => 'Atrium::Exhibit',
+    :foreign_key => 'atrium_collection_id',
+    :order => 'set_number ASC',
+    :dependent => :destroy
+  )
+
+  accepts_nested_attributes_for(
+    :exhibits,
+    :allow_destroy => true
+  )
 
   def exhibit_order
     exhibit_order = {}
@@ -68,22 +79,43 @@ class Atrium::Collection < ActiveRecord::Base
     end
   end
 
+  @@included_themes = ['default']
+  def self.available_themes
+    return @@available_themes if defined? @@available_themes
+    # NOTE: theme filenames should conform to rails expectations and only use periods to delimit file extensions
+    local_themes = Dir.entries(File.join(Rails.root, 'app/views/layouts/atrium_themes')).reject {|f| f =~ /^\./}
+    local_themes.collect!{|f| f.split('.').first}
+    @@available_themes = @@included_themes + local_themes
+  end
+
   def theme_path
     theme.blank? ? 'atrium_themes/default' : "atrium_themes/#{theme}"
   end
 
-  private
 
-  def add_collection_of_facets_by_name(collection_of_facet_names)
-    collection_of_facet_names.each do |name|
-      search_facets << Atrium::Search::Facet.find_or_create_by_name_and_collection_id(name, id)
-    end
+  serialize :filter_query_params
+
+  serialize :collection_items, Hash
+  def collection_items
+    read_attribute(:collection_items) || write_attribute(:collection_items, {})
   end
 
-  def remove_collection_of_facets_by_name(collection_of_facet_names)
-    collection_of_facet_names.each do |name|
-      search_facets.delete(Atrium::Search::Facet.find_by_name_and_collection_id(name, id))
-    end
+
+  def solr_doc_ids
+    collection_items[:solr_doc_ids] unless collection_items.blank?
   end
+
+  def pretty_title
+    title.blank? ? 'Unnamed Collection' : title
+  end
+
+  def display_title
+    has_custom_title? ? title_markup.html_safe : "<h2>#{pretty_title}</h2>".html_safe
+  end
+
+  def has_custom_title?
+    !title_markup.blank?
+  end
+  private :has_custom_title?
 
 end
