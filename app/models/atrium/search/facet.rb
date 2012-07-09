8a535704321eae31f935f7e36e61b51907459d81
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-07-09 14:45:52 -0400

    Reworking Atrium::Showcase::Item by moving methods to group concepts together.
    While the refactor looks big, I only rearranged methods – and declared one
    private. The idea being that I want to group together the methods that interact
    on the same aspect of the class.

--- app/models/atrium/search/facet.rb
+++ app/models/atrium/search/facet.rb
@@ -1,19 +1,29 @@
 class Atrium::Search::Facet < ActiveRecord::Base
   self.table_name = 'atrium_search_facets'
 
-  belongs_to :collection, :class_name => 'Atrium::Collection', :foreign_key => 'atrium_collection_id'
-
-  validates_presence_of :collection, :name
-
-  attr_accessible :atrium_collection_id, :name
+  belongs_to(
+    :collection,
+    :class_name => 'Atrium::Collection',
+    :foreign_key => 'atrium_collection_id'
+  )
 
   scope :find_by_name_and_collection_id, lambda {|name, collection_id|
     where("#{self.quoted_table_name}.`name` = ? AND #{self.quoted_table_name}.`atrium_collection_id` = ?", name, collection_id)
   }
 
   def self.find_or_create_by_name_and_collection_id(name, collection_id)
-    exsiting_facets = self.find_by_name_and_collection_id(name, collection_id)
-    exsiting_facets.any? ? exsiting_facets.first : self.create(:name => name, :atrium_collection_id => id)
+    find_by_name_and_collection_id(name, collection_id).first ||
+    self.create(:name => name, :atrium_collection_id => id)
   end
+
+  validates_presence_of(
+    :atrium_collection_id,
+    :name
+  )
+
+  attr_accessible(
+    :atrium_collection_id,
+    :name
+  )
 
 end
