d4e947c42926d9d73cfa4f01418da18dd44bbc32
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-08-02 15:34:58 -0400

    Adjusted Atrium::Search::Facet scope
    The scope was causing problems getting the tests to run; saying
    that the table wasn't defined.

--- app/models/atrium/search/facet.rb
+++ app/models/atrium/search/facet.rb
@@ -1,30 +1,26 @@
 module Atrium
   class Search::Facet < ActiveRecord::Base
-   self.table_name = 'atrium_search_facets'
+    self.table_name = 'atrium_search_facets'
 
     belongs_to(
-        :collection,
-        :class_name => 'Atrium::Collection',
-        :foreign_key => 'atrium_collection_id'
+      :collection,
+      :class_name => 'Atrium::Collection',
+      :foreign_key => 'atrium_collection_id'
     )
 
-    scope :find_by_name_and_collection_id, lambda {|name, collection_id|
-      where("#{self.quoted_table_name}.`name` = ? AND #{self.quoted_table_name}.`atrium_collection_id` = ?", name, collection_id)
-    }
-
     def self.find_or_create_by_name_and_collection_id(name, collection_id)
-      find_by_name_and_collection_id(name, collection_id).first ||
-          self.create(:name => name, :atrium_collection_id => collection_id)
+      where(name: name, atrium_collection_id: collection_id) ||
+      create(:name => name, :atrium_collection_id => collection_id)
     end
 
     validates_presence_of(
-        :atrium_collection_id,
-        :name
+      :atrium_collection_id,
+      :name
     )
 
     attr_accessible(
-        :atrium_collection_id,
-        :name
+      :atrium_collection_id,
+      :name
     )
   end
 end
