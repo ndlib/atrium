7fe14020ca5883ff7d4ee60fc4b280f5bc6ea1d4
Banurekha L <blakshmi@nd.edu>
2012-08-10 13:29:11 -0400

    routes refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Atrium::Engine.routes.draw do
   resources :collections do
-    resources :exhibits
-    resource :showcases, :only => [:new]
+    resources :exhibits do
+      member do
+        get :include_filter
+        get :exclude_filer
+      end
+    end
+    resources :showcases
   end
   resources :exhibits do
-    resource :showcases, :only => [:new]
+    resources :showcases
   end
-resources :showcases, :only => [:show]
-  resources :descriptions do
-    resources :essays
+
+  resources :showcases, :only => [:show]
+    resources :descriptions do
+      resources :essays
   end
   root :to => "collections#index"
+
 end
