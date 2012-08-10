b96b9a81414131ee2db44bb5a105f0a1ff6aa506
Banurekha L <blakshmi@nd.edu>
2012-08-10 13:27:35 -0400

    added atrium user helper methods

--- app/controllers/atrium/application_controller.rb
+++ app/controllers/atrium/application_controller.rb
@@ -1,17 +1,19 @@
 module Atrium
   class ApplicationController < ActionController::Base
     include Atrium::ApplicationHelper
-    #def atrium_config
-    #  return @atrium_config if @atrium_config
-    #  @atrium_config = OpenStruct.new
-    #  @atrium_config.facet_name_map = [['Label','Key']]
-    #
-    #  @atrium_config
-    #end
+
+    def atrium_user
+      current_user
+    end
+    helper_method :atrium_user
+
+    def sign_in_path
+      new_user_session_path
+    end
+
     def atrium_config
       Atrium.config
     end
     helper_method :atrium_config
-   # protected :atrium_config
   end
 end
