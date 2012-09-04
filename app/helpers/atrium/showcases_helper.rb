e7d79e234f7a1667824ae1b96538a777e40bfd8b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-09-04 09:30:43 -0400

    Renamed #render_filter_element
    There was a blacklight method name collision between
    Atrium::ShowcasesHelper#render_filter_element
    Blacklight::RenderConstraintsHelperBehavior#render_filter_element.
    
    Instead, replacing Atrium::ShocasesHelper#render_filter_element
    with Atrium::ShocasesHelper#render_atrium_filter_element

--- app/helpers/atrium/showcases_helper.rb
+++ app/helpers/atrium/showcases_helper.rb
@@ -11,7 +11,7 @@ module Atrium
       path=showcase.for_exhibit? ? main_app.exhibit_path(:id=>showcase.parent.id, :f=>facet) : main_app.collection_path(showcase.parent)
       return path
     end
-    
+
     def get_showcase_parent_edit_path(showcase)
       path=showcase.for_exhibit? ? edit_exhibit_showcase_path(:id=>showcase.id, :exhibit_id=>showcase.parent.id) : edit_collection_showcase_path(:id=>showcase.id, :collection_id=>showcase.parent.id)
       return path
@@ -27,13 +27,13 @@ module Atrium
       return "".html_safe unless facets
       content = []
       facets.each do |facet|
-        content << render_filter_element(facet.solr_facet_name, facet.value)
+        content << render_atrium_filter_element(facet.solr_facet_name, facet.value)
       end
 
       return content.flatten.join("\n").html_safe
     end
 
-    def render_filter_element(facet, value)
+    def render_atrium_filter_element(facet, value)
       {atrium_facet_field_labels[facet] => value}
     end
   end
