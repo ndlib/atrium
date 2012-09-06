5260e1a8250efb965b44d91b55e2652eaf7b52f4
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-09-06 09:15:30 -0400

    Atrium showcases factory now includes :showcase

--- spec/factories/atrium_showcases.rb
+++ spec/factories/atrium_showcases.rb
@@ -1,15 +1,25 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :collection_showcase, :class => 'Atrium::Showcase' do
+  factory :showcase, :class => 'Atrium::Showcase' do
     association :showcases, factory: :collection
     sequence(:tag) { |n| "tag-#{n}" }
     sequence(:sequence) { |n| n.to_i }
   end
 
-  factory :exhibit_showcase, :class => 'Atrium::Showcase' do
+  factory(
+    :collection_showcase,
+    :class => 'Atrium::Showcase',
+    :parent => :showcase
+  ) do
     association :showcases, factory: :exhibit
-    sequence(:tag) { |n| "tag-#{n}" }
-    sequence(:sequence) { |n| n.to_i }
+  end
+
+  factory(
+    :exhibit_showcase,
+    :class => 'Atrium::Showcase',
+    :parent => :showcase
+  ) do
+    association :showcases, factory: :exhibit
   end
 end
