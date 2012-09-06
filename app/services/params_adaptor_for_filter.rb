6322aeaa4d9045dc5fddba8f35491ad95f5694a5
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-09-06 14:48:37 -0400

    Cleaning up silly comments

--- app/services/params_adaptor_for_filter.rb
+++ app/services/params_adaptor_for_filter.rb
@@ -1,15 +1,15 @@
 class ParamsAdaptorForFilter < Delegator
   def initialize(obj)
-    super             # pass obj to Delegator constructor, required
-    @_sd_obj = obj    # store obj for future use
+    super
+    @_sd_obj = obj
   end
 
   def __getobj__
-    @_sd_obj          # return object we are delegating to, required
+    @_sd_obj
   end
 
   def __setobj__(obj)
-    @_sd_obj = obj    # change delegation object, a feature we're providing
+    @_sd_obj = obj
   end
 
   def filter_query_params
