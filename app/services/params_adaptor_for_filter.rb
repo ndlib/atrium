eea2064d050ae6238d545498cad32963f2e51a9b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-09-06 14:35:44 -0400

    Updated ParamsAdaptorForFilter to use Deleagator

--- app/services/params_adaptor_for_filter.rb
+++ app/services/params_adaptor_for_filter.rb
@@ -1,8 +1,18 @@
-class ParamsAdaptorForFilter < DelegateClass(Hash)
-  def initialize(params)
-    super(params)
+class ParamsAdaptorForFilter < Delegator
+  def initialize(obj)
+    super             # pass obj to Delegator constructor, required
+    @_sd_obj = obj    # store obj for future use
   end
+
+  def __getobj__
+    @_sd_obj          # return object we are delegating to, required
+  end
+
+  def __setobj__(obj)
+    @_sd_obj = obj    # change delegation object, a feature we're providing
+  end
+
   def filter_query_params
-    self
+    @_sd_obj
   end
 end
