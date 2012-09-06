c888feff94d072b62e8aa36d844564b5ab30d7b9
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-09-06 08:56:37 -0400

    Consolodated exhibit factories

--- spec/factories/atrium_exhibits.rb
+++ spec/factories/atrium_exhibits.rb
@@ -4,5 +4,6 @@ FactoryGirl.define do
   factory :exhibit, :class => 'Atrium::Exhibit' do
     association :collection
     sequence(:label) {|n| "Label #{n}" }
+    sequence(:set_number) {|n| n.to_i }
   end
 end
