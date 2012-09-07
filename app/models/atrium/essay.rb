e82378e41cf0bb664855ddf0783b2cd15b141dde
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2012-09-07 07:47:51 -0400

    Cleaning up module declaration; reclaimin 2 cols

--- app/models/atrium/essay.rb
+++ app/models/atrium/essay.rb
@@ -1,22 +1,20 @@
-module Atrium
-  class Essay < ActiveRecord::Base
-    belongs_to(
-        :description,
-        class_name: 'Atrium::Description',
-        foreign_key: 'atrium_description_id',
-        inverse_of: :essay
-    )
+class Atrium::Essay < ActiveRecord::Base
+  belongs_to(
+    :description,
+    class_name: 'Atrium::Description',
+    foreign_key: 'atrium_description_id',
+    inverse_of: :essay
+  )
 
-    validates_presence_of :description
+  validates_presence_of :description
 
-    attr_accessible(
-        :content_type,
-        :content ,
-        :atrium_description_id
-    )
+  attr_accessible(
+    :content_type,
+    :content ,
+    :atrium_description_id
+  )
 
-    def blank?
-      content.blank?
-    end
+  def blank?
+    content.blank?
   end
 end
--- app/models/atrium/exhibit.rb
+++ app/models/atrium/exhibit.rb
@@ -1,65 +1,63 @@
-module Atrium
-  class Exhibit < ActiveRecord::Base
-    attr_accessible(
-        :atrium_collection_id,
-        :label,
-        :set_number,
-        :browse_levels_attributes,
-        :showcases_attributes
-    )
+class Atrium::Exhibit < ActiveRecord::Base
+  attr_accessible(
+    :atrium_collection_id,
+    :label,
+    :set_number,
+    :browse_levels_attributes,
+    :showcases_attributes
+  )
 
-    belongs_to(
-        :collection,
-        class_name: 'Atrium::Collection',
-        foreign_key: 'atrium_collection_id'
-    )
+  belongs_to(
+    :collection,
+    class_name: 'Atrium::Collection',
+    foreign_key: 'atrium_collection_id'
+  )
 
-    include Atrium::IsShowcasedMixin
+  include Atrium::IsShowcasedMixin
 
-    has_many(
-        :browse_levels,
-        class_name: 'Atrium::BrowseLevel',
-        foreign_key: 'atrium_exhibit_id',
-        order: 'level_number ASC'
-    )
+  has_many(
+    :browse_levels,
+    class_name: 'Atrium::BrowseLevel',
+    foreign_key: 'atrium_exhibit_id',
+    order: 'level_number ASC'
+  )
 
-    accepts_nested_attributes_for :browse_levels, allow_destroy: true
-    def browse_facet_names
-      browse_levels.collect {|facet| facet.solr_facet_name} rescue []
-    end
+  accepts_nested_attributes_for :browse_levels, allow_destroy: true
+  def browse_facet_names
+    browse_levels.collect {|facet| facet.solr_facet_name} rescue []
+  end
 
-    def facet_order
-      facet_order = {}
-      browse_levels.map{|facet| facet_order[facet[:id]] = facet.level_number }
-      facet_order
-    end
+  def facet_order
+    facet_order = {}
+    browse_levels.map{|facet| facet_order[facet[:id]] = facet.level_number }
+    facet_order
+  end
 
-    def facet_order=(facet_order = {})
-      valid_ids = browse_levels.select(:id).map{|facet| facet[:id]}
-      facet_order.each_pair do |id, order|
-        if valid_ids.include?(id.to_i)
-          Atrium::BrowseLevel.find(id).update_attributes!(level_number: order)
-        end
+  def facet_order=(facet_order = {})
+    valid_ids = browse_levels.select(:id).map{|facet| facet[:id]}
+    facet_order.each_pair do |id, order|
+      if valid_ids.include?(id.to_i)
+        Atrium::BrowseLevel.find(id).update_attributes!(level_number: order)
       end
     end
+  end
 
-    include Atrium::QueryParamMixin
+  include Atrium::QueryParamMixin
 
-    def pretty_title
-      label.blank? ? "Exhibit #{set_number}" : label
-    end
+  def pretty_title
+    label.blank? ? "Exhibit #{set_number}" : label
+  end
 
-    def get_available_facets
-      collection.search_facet_names - browse_facet_names
-    end
+  def get_available_facets
+    collection.search_facet_names - browse_facet_names
+  end
 
-    before_create :assign_set_number
+  before_create :assign_set_number
 
-    private
-    def assign_set_number
-      if collection
-        self.set_number= collection.exhibits.size + 1
-      end
+  private
+  def assign_set_number
+    if collection
+      self.set_number= collection.exhibits.size + 1
     end
   end
 end
